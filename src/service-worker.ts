3469a8763791e8e8ffa2655b613edf0247099aa5
EseMonday <mondayese8@gmail.com>
2024-02-21 19:43:06 +0100

    webpack

---
+++ src/service-worker.ts
@@ -0,0 +1,6 @@
+import { precacheAndRoute } from 'workbox-precaching';
+
+declare const self: ServiceWorkerGlobalScope;
+
+// Precache assets using Workbox
+precacheAndRoute(self.__WB_MANIFEST || []);
