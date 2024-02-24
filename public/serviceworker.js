e98a1e54397a6b4154c224c4f2eb448017fce65e
EseMonday <mondayese8@gmail.com>
2024-02-25 00:23:49 +0100

    publish

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,66 +1,51 @@
-const CACHE_NAME = "version1";
+const CACHE_NAME = "version 1"
 
-const urlsToCache = [
-    "index.html", 
-    "offline.html",
-    "./manifest.json",
-    "./android-chrome-192x192.png",
-    "./",
-    ".header-bg.png",
-    "./favicon.ico",
-    "./static/js/main.4d5113ea.js",
-    "./static/js/bundle.js",
-    "./static/css/main.073c9b0a.css",
-    "/city-list",
-    "/city-details",
-    "/search"
-];
-const self = this;
+const urlsToCache =[
+    "index.html", "offline.html"
+]
 
-self.addEventListener("install", (event) => {
+const self = this
+//Installation
+
+self.addEventListener("install", (event) =>{
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log("Cache Open");
-            return cache.addAll(urlsToCache);
+        .then((cache) =>{
+            console.log("Cache Open")
+            return cache.addAll(urlsToCache)
         })
-    );
+    )
 });
 
-function fromNetwork(request, timeout) {
-    return new Promise(function (fulfill, reject) {
-        var timeoutId = setTimeout(reject, timeout);
-        fetch(request).then(function (response) {
-            clearTimeout(timeoutId);
-            fulfill(response);
-        }).catch(reject);
-    });
-}
 
-function fromCache(request) {
-    return caches.open(CACHE_NAME).then(function (cache) {
-        return cache.match(request).then(function (matching) {
-            return matching || Promise.reject('no-match');
-        });
-    });
-}
+//Listen for Request
+self.addEventListener("fetch", (event) =>{
+    event.respondWith(
+        caches.match(event.request)
+        .then(() => {
+            return fetch(event.request)
+            .catch(() =>  caches.match('offline.html'))
+        })
+    )
 
-self.addEventListener('fetch', function (evt) {
-    evt.respondWith(
-        fromNetwork(evt.request, 400).catch(() => fromCache(evt.request))
-    );
 });
 
-self.addEventListener("activate", (event) => {
-    const cacheWhiteList = [CACHE_NAME];
+
+
+//ACtivate Service worker
+self.addEventListener("activate", (event) =>{
+    const cacheWhiteList = []
+    cacheWhiteList.push(CACHE_NAME)
 
     event.waitUntil(
         caches.keys().then((cacheNames) => Promise.all(
             cacheNames.map((cacheName) => {
-                if (!cacheWhiteList.includes(cacheName)) {
+                if(!cacheWhiteList.includes(cacheName)){
                     return caches.delete(cacheName);
                 }
+                // return cacheName
             })
         ))
-    );
+    )
+
 });
